968d383187887545e1c41bff60dc5095fdd9748b
donato-fiore <50346119+donato-fiore@users.noreply.github.com>
2020-05-19 21:56:00 -0400

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,12 +1,14 @@
-from selenium import webdriver
 import time
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 print("[1] Bee Movie Script")
 print("[2] Never Gonna Give You Up")
 print("[3] Shrek Movie Script")
 print("[4] Africa Lyrics")
 print("[5] Rappin for Jesus")
+print("[6] bbboob, bbgif, bbass")
 script = input("Choose Spam Method: ")
 
 driverpath="/bin/chromedriver"
@@ -15,14 +17,14 @@ driver.get('https://discord.com/login')
 
 time.sleep(5)
 emailogin = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/div[1]/div/input')
-emailogin.send_keys('EMAIL')
+emailogin.send_keys('donato.v.fiore@gmail.com')
 
 passlogin = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/div[2]/div/input')
-passlogin.send_keys('PASSWORD')
+passlogin.send_keys('DahQGgHcyk5842K')
 
 driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]').click()
-time.sleep(25)
-driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[1]/nav/div[2]/div/div/a[3]/div/div[2]/div[1]/div').click()
+
+channel = str('https://discord.com/channels/711349040469508147/712189660528640040')
 
 if script == "1":
     f = open('scripts/beemovie.txt', 'r')
@@ -34,17 +36,24 @@ elif script == "4":
     f = open('scripts/africa.txt', 'r')
 elif script == "5":
     f = open('scripts/jesus.txt', 'r')
+elif script == "6":
+    f = open('scripts/bb.txt', 'r')
+    channel = str('https://discord.com/channels/711349040469508147/711350457792200797')
 
-elem = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div[2]/main/form/div/div/div/div[3]/div[2]')
+time.sleep(25)
+driver.get(channel)
+time.sleep(5)
+
+time.sleep(30)
+elem = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div/main/form/div/div/div/div/div[3]/div[1]')
 
 TypeError = False
+
 for line in f:
     try:
-        elem = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div[2]/main/form/div/div/div/div[3]/div[2]')
-        elem.send_keys(line)
-        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div[2]/main/form/div/div/div/div[3]/div[2]').send_keys(Keys.ENTER)
-        time.sleep(2.5)
+        actions = ActionChains(driver)
+        actions.send_keys(line, Keys.ENTER)
+        actions.perform()
         print("[+] Message sent: %s" % line)
     except BaseException:
         print("[!] Message not sent: %s" % line)
-        break;      
